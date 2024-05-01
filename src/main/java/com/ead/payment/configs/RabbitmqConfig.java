8decdeac2ccca42a0d9ce2fa23a2bd2b6657f625
Idevaldo Neto <inetomec@yahoo.com.br>
2024-05-01 15:55:17 -0300

    chore: Adicionadas configurações do Rabbitmq

---
+++ src/main/java/com/ead/payment/configs/RabbitmqConfig.java
@@ -0,0 +1,36 @@
+package com.ead.payment.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    private final CachingConnectionFactory cachingConnectionFactory;
+
+    @Autowired
+    public RabbitmqConfig(final CachingConnectionFactory cachingConnectionFactory) {
+        this.cachingConnectionFactory = cachingConnectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        final RabbitTemplate template = new RabbitTemplate(this.cachingConnectionFactory);
+        template.setMessageConverter(this.messageConverter());
+        return template;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+}
