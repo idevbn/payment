514dc7df86c2287887b19e4aba8d7079f922ccd0
Idevaldo Neto <inetomec@yahoo.com.br>
2024-05-02 18:21:30 -0300

    feat: Criado consumer UserConsumer

---
+++ src/main/java/com/ead/payment/consumers/UserConsumer.java
@@ -0,0 +1,52 @@
+package com.ead.payment.consumers;
+
+import com.ead.payment.dtos.UserEventDTO;
+import com.ead.payment.enums.ActionType;
+import com.ead.payment.models.UserModel;
+import com.ead.payment.services.UserService;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserConsumer {
+
+    private final UserService userService;
+
+    public UserConsumer(final UserService userService) {
+        this.userService = userService;
+    }
+
+    @RabbitListener(
+            bindings = @QueueBinding(
+                    value = @Queue(value = "${ead.broker.queue.userEventQueue.name}", durable = "true"),
+                    exchange = @Exchange(
+                            value = "${ead.broker.exchange.userEventExchange}",
+                            type = ExchangeTypes.FANOUT,
+                            ignoreDeclarationExceptions = "true"
+                    )
+            )
+    )
+    public void listenUserEvent(@Payload final UserEventDTO userEventDTO) {
+        final UserModel userModel = userEventDTO.convertToUserModel();
+
+        switch (ActionType.valueOf(userEventDTO.getActionType())) {
+            case CREATE:
+                this.userService.save(userModel);
+                break;
+
+            case UPDATE:
+                this.userService.save(userModel);
+                break;
+
+            case DELETE:
+                this.userService.delete(userModel.getUserId());
+                break;
+        }
+    }
+
+}
