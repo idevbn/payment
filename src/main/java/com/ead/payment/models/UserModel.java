0cc1d39a1f8dc9b0a80722dc04f414fbffda92ef
Idevaldo Neto <inetomec@yahoo.com.br>
2024-05-02 18:17:23 -0300

    feat: Criado model referente ao Usuário

---
+++ src/main/java/com/ead/payment/models/UserModel.java
@@ -0,0 +1,46 @@
+package com.ead.payment.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_users")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private UUID userId;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 150)
+    private String fullName;
+
+    @Column(nullable = false)
+    private String userStatus;
+
+    @Column(nullable = false)
+    private String userType;
+
+    @Column(length = 20)
+    private String cpf;
+
+    @Column(length = 20)
+    private String phoneNumber;
+
+}
