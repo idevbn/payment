c7da698971d419612fd079c7285bbc2c9b645468
Idevaldo Neto <inetomec@yahoo.com.br>
2024-05-03 10:40:03 -0300

    feat: Criado enum PaymentStatus

---
+++ src/main/java/com/ead/payment/enums/PaymentStatus.java
@@ -0,0 +1,13 @@
+package com.ead.payment.enums;
+
+/**
+ * Enum que representa o status do pagamento com relação ao usuário.
+ */
+public enum PaymentStatus {
+
+    NOTSTARTED,
+    PAYING,
+    DEBTOR,
+    CANCELED;
+
+}
