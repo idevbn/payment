658655828098d313f96305e34946956d16195d6c
Idevaldo Neto <inetomec@yahoo.com.br>
2024-05-03 15:30:07 -0300

    feat: Criada entidade de pagamentos

---
+++ src/main/java/com/ead/payment/models/PaymentModel.java
@@ -0,0 +1,59 @@
+package com.ead.payment.models;
+
+import com.ead.payment.enums.PaymentStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_payments")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PaymentModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    @Column(nullable = false)
+    private LocalDateTime paymentRequestDate;
+
+    @Column
+    private LocalDateTime paymentCompletionDate;
+
+    @Column(nullable = false)
+    private LocalDateTime paymentExpirationDate;
+
+    @Column(nullable = false, length = 4)
+    private String lastDigitsCreditCard;
+
+    @Column(nullable = false)
+    private BigDecimal valuePaid;
+
+    @Column(length = 150)
+    private String paymentMessage;
+
+    @Column
+    private boolean recurrence;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private UserModel user;
+
+}
