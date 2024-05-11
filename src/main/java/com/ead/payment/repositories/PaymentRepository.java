8e58fc92c5720cda4760c9a1d4ff25434726f61f
Idevaldo Neto <inetomec@yahoo.com.br>
2024-05-11 11:42:18 -0300

    feat: Criado método cuja query retorna um pagamento por usuário

--- src/main/java/com/ead/payment/repositories/PaymentRepository.java
+++ src/main/java/com/ead/payment/repositories/PaymentRepository.java
@@ -4,6 +4,8 @@ import com.ead.payment.models.PaymentModel;
 import com.ead.payment.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -11,5 +13,12 @@ import java.util.UUID;
 public interface PaymentRepository extends JpaRepository<PaymentModel, UUID>, JpaSpecificationExecutor<PaymentModel> {
 
     Optional<PaymentModel> findTopByUserOrderByPaymentRequestDateDesc(final UserModel userModel);
+
+    @Query(value = "SELECT * FROM tb_payments WHERE user_user_id = :userId AND payment_id = :paymentId",
+    nativeQuery = true)
+    Optional<PaymentModel> findPaymentByUser(
+            @Param(value = "userId") final UUID userId,
+            @Param(value = "paymentId") final UUID paymentId
+    );
 
 }
