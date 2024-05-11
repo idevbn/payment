4df7f39926b7c3437cb60a10b43ac674a1b39f08
Idevaldo Neto <inetomec@yahoo.com.br>
2024-05-11 00:40:45 -0300

    feat: Criado DTO de entrada com dados de requisição de um pagamento

---
+++ src/main/java/com/ead/payment/dtos/PaymentRequestDTO.java
@@ -0,0 +1,36 @@
+package com.ead.payment.dtos;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
+
+@Data
+public class PaymentRequestDTO {
+
+    @NotNull
+    @Digits(integer = 5, fraction = 2)
+    @DecimalMin(value = "0.0", inclusive = false)
+    private BigDecimal valuePaid;
+
+    @NotBlank
+    private String cardHolderFullName;
+
+    @CPF
+    @NotBlank
+    private String cardHolderCpf;
+
+    @NotBlank
+    @Size(min = 16, max = 20)
+    private String creditCardNumber;
+
+    @NotBlank
+    @Size(min = 4, max = 10)
+    private String expirationDate;
+
+    @NotBlank
+    @Size(min = 3, max = 3)
+    private String cvvCode;
+
+}
