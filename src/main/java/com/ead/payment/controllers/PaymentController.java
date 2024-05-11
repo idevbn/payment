b61efdeacbc2a683bd8f84366548d159fbdaf0fa
Idevaldo Neto <inetomec@yahoo.com.br>
2024-05-11 11:44:28 -0300

    feat: Criados métodos Get da API

--- src/main/java/com/ead/payment/controllers/PaymentController.java
+++ src/main/java/com/ead/payment/controllers/PaymentController.java
@@ -6,6 +6,11 @@ import com.ead.payment.models.PaymentModel;
 import com.ead.payment.models.UserModel;
 import com.ead.payment.services.PaymentService;
 import com.ead.payment.services.UserService;
+import com.ead.payment.specifications.SpecificationTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -62,6 +67,36 @@ public class PaymentController {
                 .requestPaymentStatus(paymentRequestDTO, userModelOptional.get());
 
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(paymentModel);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("users/{userId}/payments")
+    public ResponseEntity<Page<PaymentModel>> getAllPayments(
+            @PathVariable(value = "userId") final UUID userId,
+            final SpecificationTemplate.PaymentSpec spec,
+            @PageableDefault(page = 0, size = 10, sort = "paymentId", direction = Sort.Direction.DESC)
+            final Pageable pageable
+            ) {
+        final Page<PaymentModel> paymentModelPage = this.paymentService
+                .findAllByUser(SpecificationTemplate.paymentUserId(userId).and(spec), pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(paymentModelPage);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("users/{userId}/payments/{paymentId}")
+    public ResponseEntity<Object> getOnePayment(
+            @PathVariable(value = "userId") final UUID userId,
+            @PathVariable(value = "paymentId") final UUID paymentId
+    ) {
+        final Optional<PaymentModel> paymentModelOptional = this.paymentService
+                .findPaymenyByUser(userId, paymentId);
+
+        if (paymentModelOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Payment nof found for this user.");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(paymentModelOptional.get());
     }
 
 }
