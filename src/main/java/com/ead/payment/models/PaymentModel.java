e1e279932dfaa19aa911feb9bc91f9749e4d16ec
Idevaldo Neto <inetomec@yahoo.com.br>
2024-05-28 23:38:12 -0300

    feat: Criado método que converte um PaymentModel em PaymentEventDTO

--- src/main/java/com/ead/payment/models/PaymentModel.java
+++ src/main/java/com/ead/payment/models/PaymentModel.java
@@ -1,5 +1,6 @@
 package com.ead.payment.models;
 
+import com.ead.payment.dtos.PaymentEventDTO;
 import com.ead.payment.enums.PaymentControl;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -7,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.BeanUtils;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -57,5 +59,15 @@ public class PaymentModel implements Serializable {
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private UserModel user;
+
+    public PaymentEventDTO convertToPaymentEventDTO() {
+        final PaymentEventDTO paymentEventDTO = new PaymentEventDTO();
+
+        BeanUtils.copyProperties(this, paymentEventDTO);
+        paymentEventDTO.setPaymentControl(this.getPaymentControl().toString());
+        paymentEventDTO.setUserId(this.getUser().getUserId());
+
+        return paymentEventDTO;
+    }
 
 }
